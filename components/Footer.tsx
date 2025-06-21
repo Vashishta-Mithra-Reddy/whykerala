1c892e72370de0ba159c2a2666124e538c02a136
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-06-21 17:27:10 +0530

    Adsf

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className="flex items-center justify-between mx-auto h-32 md:h-28 px-16 md:px-40 md:pb-0 bg-whitex/95 dark:bg-whitex/10 ">
             <Link href="/" className="text-2xl font-semibold font-satoshi text-redx/80">
-            We'll go keralaaa.
+            Let's go keralaaa.
             </Link>
             <ThemeSwitcher />
         </div>
