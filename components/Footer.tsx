ac511f7f97762b143ab67c16386ffc110c353070
Vashishta Mithra Reddy Vundyala <vashishtamithrareddy@gmail.com>
2025-06-22 18:08:31 +0530

    footer height fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import ThemeSwitcher from "./theme-switcher";
 
 export default function Footer() {
     return (
-        <div className="flex items-center justify-center mx-auto h-28 md:h-32 px-16 md:px-40 md:pb-0 bg-whitex/95 dark:bg-whitex/10 ">
+        <div className="flex items-center justify-center mx-auto h-28 md:h-28 px-16 md:px-40 md:pb-0 bg-whitex/95 dark:bg-whitex/10 ">
             <Link href="/" className="text-2xl font-semibold font-satoshi text-redx/80">
             Let's go to keralaaa.
             </Link>
